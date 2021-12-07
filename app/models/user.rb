8825a8bcb148cecfe613b234f22df4416fdff4dc
伊東和成 <itokazunari5375@icloud.com>
2021-12-07 10:25:44 +0900

    create function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # フォローする側から中間テーブルへのアソシエーション
+  has_many :relationships, foreign_key: :following_id
+  # フォローする側からフォローされたユーザを取得する
+  has_many :followings, through: :relationships, source: :follower
+
+  # フォローされる側から中間テーブルへのアソシエーション
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # フォローされる側からフォローしているユーザを取得する
+  has_many :followers, through: :reverse_of_relationships, source: :following
 end
