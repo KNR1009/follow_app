7c4a7aac977e0eaae41b685558e9a3d759b546ad
伊東和成 <itokazunari5375@icloud.com>
2021-12-12 19:36:54 +0900

    user情報のviewを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
   # フォローされる側からフォローしているユーザを取得する
   has_many :followers, through: :reverse_of_relationships, source: :following
+
+    # あるユーザが引数で渡されたuserにフォローされているか調べるメソッド
+    def is_followed_by?(user)
+      reverse_of_relationships.find_by(following_id: user.id).present?
+    end
 end
